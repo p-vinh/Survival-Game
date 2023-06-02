e752347a8b3ac40efeb150fd120fe94feeff9cb1
random-debug <85326677+p-vinh@users.noreply.github.com>
2023-06-01 19:31:05 -0700

    world generation changes

--- Assets/Scripts/WorldGeneration.cs
+++ Assets/Scripts/WorldGeneration.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class WorldGeneration : MonoBehaviour
 {
-    public Sprite sprite;
+
+    public maxX = 100;
+    public minX = -100;
+    public maxY = 100;
+    public minY = -100;
+ 
+    public TileMap topMap;
+    public TileMap bottomMap;
+
+    public Tile topTile;
+    public Tile bottomTile;
+    
     // Start is called before the first frame update
     void Start()
     {
